d47aec81a9f71a488a805baf61ea7ed66b6d3c6c
zhangy1053 <xinxiyang008@126.com>
2018-05-09 17:07:35 +0800

    add pair fail layout

---
+++ app/src/main/java/com/ai_keys/iot/ui/device/PairFailActivity.java
@@ -0,0 +1,15 @@
+package com.ai_keys.iot.ui.device;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.ai_keys.iot.R;
+
+public class PairFailActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pair_failure);
+    }
+}
